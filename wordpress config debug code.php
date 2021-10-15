5b5707aaca040fba656f452b165ec80ed31d0253
Ghulam Qadir <68844510+Ghulam-Qadir@users.noreply.github.com>
2021-10-16 03:53:52 +1300

    wordpress envirment

---
+++ wordpress config debug code.php
@@ -0,0 +1,8 @@
+<?php
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_DISPLAY', true );
+define( 'WP_DEBUG_LOG', true);
+define( 'SCRIPT_DEBUG', true );
+/*define( 'DISALLOW_FILE_EDIT', true );
+define( 'DISALLOW_FILE_MODS', true );*/ 
+ ?>
