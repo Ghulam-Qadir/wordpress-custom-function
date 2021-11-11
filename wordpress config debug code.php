80072d4426e91c8b524b3e84a2e2a3b5bf45c126
Ghulam Qadir <68844510+Ghulam-Qadir@users.noreply.github.com>
2021-11-11 14:17:11 +0500

    another update

--- wordpress config debug code.php
+++ wordpress config debug code.php
@@ -5,4 +5,11 @@ define( 'WP_DEBUG_LOG', true);
 define( 'SCRIPT_DEBUG', true );
 /*define( 'DISALLOW_FILE_EDIT', true );
 define( 'DISALLOW_FILE_MODS', true );*/ 
+// All Update Remove
+function remove_core_updates(){
+global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
+}
+add_filter('pre_site_transient_update_core','remove_core_updates');
+add_filter('pre_site_transient_update_plugins','remove_core_updates');
+add_filter('pre_site_transient_update_themes','remove_core_updates');
  ?>
